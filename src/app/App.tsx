661b119e60cdae25f55ef87cb38af1a3dd930cd2
Seydina Thiam <seydinat235@gmail.com>
2026-08-10 06:59:21 +0000

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <p className="text-3xl text-red-500 font-bold underline">
+        Hello, Vite + React!
+      </p>
+    </div>
+  )
+}
+
+export default App
